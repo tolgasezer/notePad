5a1a05e9ac04033ebda5fa92e89474416e1e25bc
tolgaszr <tolgaszr@gmail.com>
2023-06-21 14:58:25 +0300

    new prj started

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+//alert('hellooo');
+function toggleDescription(index) {
+    var description = document.getElementById(index);
+    description.style.display = (description.style.display === 'none') ? 'block' : 'none';
+  }
