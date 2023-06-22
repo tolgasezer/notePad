61d03e42d110c2cd0a9dec0e80f3e447cf0d17ec
tolgaszr <tolgaszr@gmail.com>
2023-06-22 16:39:50 +0300

    need to template tag

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,12 @@
-//alert('hellooo');
-function toggleDescription(index) {
-    var description = document.getElementById(index);
-    description.style.display = (description.style.display === 'none') ? 'block' : 'none';
-  }
+const addNoteBtn = document.querySelector('.notes_add');
+const notesListArea = document.querySelector('.notes_list');
+const notesListEl = document.querySelector('.notes_list-item');
+console.log(notesListEl);
+
+const createNewNote = () =>{
+    const copyNotesListEl = notesListEl.cloneNode(true);
+    notesListArea.append(copyNotesListEl);
+};
+
+
+addNoteBtn.addEventListener('click',createNewNote);
